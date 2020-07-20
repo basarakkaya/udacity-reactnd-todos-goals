f9e4aa48ff2ea3fbeb048d367a33dd9b242c2d30
Osman Başar Akkaya <osmanbasar.akkaya@tokeninc.com>
2020-07-20 08:50:24 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+
+import Todos from "./Todos";
+import Goals from "./Goals";
+
+class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    dispatch(handleInitialData());
+  }
+  render() {
+    if (this.props.loading === true) {
+      return <h3>Loading</h3>;
+    }
+
+    return (
+      <div>
+        <Todos />
+        <Goals />
+      </div>
+    );
+  }
+}
+
+export default connect((state) => ({
+  loading: state.loading,
+}))(App);
